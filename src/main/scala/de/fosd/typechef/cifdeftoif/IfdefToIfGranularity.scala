c46cb56fefc083ab43dacab0992b79d706771b73
ChristianKapfhammer <christian_kapfhammer@gmx.de>
2017-06-16 12:35:25 +0200

    Adjustment of score

--- src/main/scala/de/fosd/typechef/cifdeftoif/IfdefToIfGranularity.scala
+++ src/main/scala/de/fosd/typechef/cifdeftoif/IfdefToIfGranularity.scala
@@ -1256,7 +1256,7 @@ trait IfdefToIfGranularityExecCode extends IfdefToIfGranularityInterface with IO
                         }
                         obj match {
                             case i: IfStatement =>
-                                adjustedWeight = weight/Math.pow(1.5, i.elifs.size+2)
+                                adjustedWeight = weight/Math.pow(1.75, i.elifs.size+2)
                             case SwitchStatement(_, One(CompoundStatement(list))) =>
                                 var amountCases = 0
 
